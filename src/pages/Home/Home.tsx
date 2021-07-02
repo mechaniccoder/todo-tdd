821a8fdb7922bd45179a71aeb6d7b77f9072bb42
mechaniccoder <yuseunghwan94@gmail.com>
2021-07-02 23:32:24 +0900

    Ignore coverage folder

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  con;
   return (
     <div>
       <div>go to sign up</div>
+      <button>submit</button>
       {/* <Link to="/signup">go to sign up</Link> */}
     </div>
   );
