cfbb6be8c007b00666f30c78ba5c0e4d228867ad
mechaniccoder <yuseunghwan94@gmail.com>
2021-07-07 00:14:51 +0900

    Test TodoList component

--- todo/components/TodoList/TodoList.test.tsx
+++ todo/components/TodoList/TodoList.test.tsx
@@ -1,7 +1,24 @@
+import { render } from '@testing-library/react';
 import { useAppSelector } from 'hooks/useReactRedux';
+import { Todo } from 'store/TodoSlice';
+import TodoList from './TodoList';
 
+jest.mock('hooks/useReactRedux');
 describe('TodoList', () => {
   describe('TodoList ', () => {
-    it('renders todo list', () => {});
+    const useAppSelectorMock = useAppSelector as jest.Mock;
+    it('renders todo list', () => {
+      useAppSelectorMock.mockImplementation((selector) =>
+        selector({
+          todo: [
+            { text: 'TDD를 배우자', done: false },
+            { text: 'Next를 배우자', done: false },
+          ],
+        } as { todo: Todo[] }),
+      );
+      const { container } = render(<TodoList />);
+      expect(container).toHaveTextContent('TDD를 배우자');
+      expect(container).toHaveTextContent('TDD를 배우자');
+    });
   });
 });
--- todo/components/TodoList/TodoList.tsx
+++ todo/components/TodoList/TodoList.tsx
@@ -2,7 +2,16 @@ import { useAppSelector } from 'hooks/useReactRedux';
 
 const TodoList = () => {
   const todo = useAppSelector(({ todo }) => todo);
-  return <div></div>;
+  return (
+    <ul>
+      {todo.map((t) => (
+        <li key={t.text}>
+          <input type="checkbox" defaultChecked={t.done} />
+          <p>{t.text}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
